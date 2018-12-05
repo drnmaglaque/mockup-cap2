bc9662f8c7a6d4dac6b3c65c75c1f75ac400cd3d
Darren Maglaque <darren.maglaque@gmail.com>
2018-12-05 16:48:19 +0800

    second update

--- script.js
+++ script.js
@@ -3,5 +3,21 @@ $(document).ready(function(){
 	$(".top-notification").click(function(){
 		$(".top-notification").toggleClass("hide");
 		})
+
+	$(".show-cart-btn").click(function(){
+		$(".cart-section").removeClass("hide-cart");
+		$(".cart-section").addClass("show-cart");
 	})
 
+	$(".close-cart-btn").click(function(){
+		$(".cart-section").removeClass("show-cart");
+		$(".cart-section").addClass("hide-cart");
+	})
+	
+
+
+
+
+	})
+
+	
